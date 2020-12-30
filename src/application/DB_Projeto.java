6d991e4d78e8c6f6b247a096578cd15c8377c8d7
Gabriel Bessa <bessagabriel490@gmail.com>
2020-12-29 22:06:30 -0300

    Classe Departamento

--- src/application/DB_Projeto.java
+++ src/application/DB_Projeto.java
@@ -1,8 +1,11 @@
 package application;
 
+import Model.entities.Departamento;
+
 public class DB_Projeto {
     public static void main(String[] args) {
-       
+       Departamento dp = new Departamento(1, "Books");
+        System.out.println(dp);
     }
 
 }
