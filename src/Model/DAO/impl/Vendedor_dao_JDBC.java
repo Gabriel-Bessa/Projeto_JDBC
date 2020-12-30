ba15f5e571575f0862532afe2e12dfaf6d8517af
Gabriel Bessa <bessagabriel490@gmail.com>
2020-12-30 02:35:56 -0300

    Melhoria no FindByID

--- src/Model/DAO/impl/Vendedor_dao_JDBC.java
+++ src/Model/DAO/impl/Vendedor_dao_JDBC.java
@@ -10,27 +10,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class Vendedor_dao_JDBC implements Vendedor_dao{
+public class Vendedor_dao_JDBC implements Vendedor_dao {
 
     private Connection con;
 
     public Vendedor_dao_JDBC(Connection con) {
         this.con = con;
     }
-    
+
     @Override
     public void insert(Vendedor obj) {
-       
+
     }
 
     @Override
     public void update(Vendedor obj) {
-        
+
     }
 
     @Override
     public void deleteById(Integer id) {
-        
+
     }
 
     @Override
@@ -39,35 +39,26 @@ public class Vendedor_dao_JDBC implements Vendedor_dao{
         ResultSet rs = null;
         try {
             st = con.prepareStatement(""
-                    + "SELECT seller.*,department.Name as DepName " +
-                        "FROM seller INNER JOIN department " +
-                        "ON seller.DepartmentId = department.Id " +
-                        "WHERE seller.Id = ?");
+                    + "SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE seller.Id = ?");
             st.setInt(1, id);
-            
+
             rs = st.executeQuery();
-            if(rs.next()){
-                Departamento dp = new Departamento();
-                dp.setId(rs.getInt("DepartmentId"));
-                dp.setNome(rs.getString("DepName"));
-                
-                Vendedor vend = new Vendedor();
-                
-                vend.setDataNacimento(rs.getDate("BirthDate"));
-                vend.setDepart(dp);
-                vend.setEmail(rs.getString("Email"));
-                vend.setId(rs.getInt("Id"));
-                vend.setNome(rs.getString("Name"));
-                vend.setSalarioBase(rs.getDouble("BaseSalary"));
-                
+            if (rs.next()) {
+                Departamento dp = instaciarDepartamento(rs);
+
+                Vendedor vend = instanciarVendedor(rs, dp);
+
                 return vend;
                 // Vendedor vend = new Vendedor(id, nome, email, DataNacimento, Double.MAX_VALUE, dp);
-            }else {
+            } else {
                 return null;
             }
         } catch (SQLException e) {
             throw new db.dbException(e.getMessage());
-        }finally {            
+        } finally {
             DB.closeResultSet(rs);
             DB.closeStatement(st);
         }
@@ -77,5 +68,26 @@ public class Vendedor_dao_JDBC implements Vendedor_dao{
     public List<Vendedor> findAll() {
         return null;
     }
+
+    private Departamento instaciarDepartamento(ResultSet rs) throws SQLException {
+        Departamento dp = new Departamento();
+        dp.setId(rs.getInt("DepartmentId"));
+        dp.setNome(rs.getString("DepName"));
+
+        return dp;
+    }
+
+    private Vendedor instanciarVendedor(ResultSet rs, Departamento dp) throws SQLException {
+        Vendedor vend = new Vendedor();
+
+        vend.setDataNacimento(rs.getDate("BirthDate"));
+        vend.setDepart(dp);
+        vend.setEmail(rs.getString("Email"));
+        vend.setId(rs.getInt("Id"));
+        vend.setNome(rs.getString("Name"));
+        vend.setSalarioBase(rs.getDouble("BaseSalary"));
     
+        return vend;
+    }
+
 }
